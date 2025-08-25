6b9b4cd7d886edc7f6618b960c670e46c3433265
[dyad] <Budianto5804@gmail.com>
2025-08-25 23:08:17 +0700

    [dyad] Memperbaiki error React.Children.only pada TooltipProvider - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,30 @@ import VoucherPage from "./pages/vouchers";
 import InputVouchersPage from "./pages/InputVouchers";
 import MoveVouchersPage from "./pages/MoveVouchers";
 import ManualStockAdjustmentPage from "./pages/ManualStockAdjustment";
-import SoldVouchersPage from "./pages/SoldVouchersPage"; // Import halaman baru
+import SoldVouchersPage from "./pages/SoldVouchersPage";
+import React from "react"; // Import React untuk Fragment
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/vouchers" element={<VoucherPage />} />
-          <Route path="/input-vouchers" element={<InputVouchersPage />} />
-          <Route path="/move-vouchers" element={<MoveVouchersPage />} />
-          <Route path="/manual-stock-adjustment" element={<ManualStockAdjustmentPage />} />
-          <Route path="/sold-vouchers" element={<SoldVouchersPage />} /> {/* Rute baru */}
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <React.Fragment> {/* Membungkus semua anak dalam satu Fragment */}
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/vouchers" element={<VoucherPage />} />
+            <Route path="/input-vouchers" element={<InputVouchersPage />} />
+            <Route path="/move-vouchers" element={<MoveVouchersPage />} />
+            <Route path="/manual-stock-adjustment" element={<ManualStockAdjustmentPage />} />
+            <Route path="/sold-vouchers" element={<SoldVouchersPage />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </React.Fragment> {/* Menutup Fragment */}
     </TooltipProvider>
   </QueryClientProvider>
 );
